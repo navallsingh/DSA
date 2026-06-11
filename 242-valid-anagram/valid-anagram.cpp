427f4060f36a87455bdc8dc0738734dcad1d26aa
Naval Singh <navalsingh8321@gmail.com>
2026-06-11 15:15:10 +0530

    Time: 8 ms (6.96%) | Memory: 9.7 MB (49.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-    if(s.size()!=t.size()) return false;
-
-    vector<int> freq(26,0);
-
-    for(char c : s) freq[c-'a']++;
-    for(char c : t) freq[c-'a']--;
-
-    for(int x : freq)
-        if(x!=0) return false;
-
-    return true;
+    if(s.length() != t.length() ){
+        return false;
+    }
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+    return s == t;
 }
 
 };
