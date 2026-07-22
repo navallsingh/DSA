894ca9881f41fd5b2a1b06cef732c5dd3b62deff
Naval Singh <navalsingh8321@gmail.com>
2026-07-22 23:18:36 +0530

    Time: 32 ms (79.85%) | Memory: 82.5 MB (86.84%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-       unordered_set <int> seen;
+      sort(nums.begin(), nums.end());
 
-       for(int num : nums){
-        if(seen.count(num))
-        return true;
-
-        seen.insert(num);
-       }
-       return false;
+      for(int i = 1;i<nums.size();i++){
+        if(nums[i] == nums[i - 1]){
+            return true;
+        }
+      }
+      return false;
     }
 };
