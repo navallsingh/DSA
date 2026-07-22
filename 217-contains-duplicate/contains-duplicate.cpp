096dca86d8192697579c67df6c6333df7937bbcd
Naval Singh <navalsingh8321@gmail.com>
2026-07-22 23:25:03 +0530

    Time: 26 ms (85.87%) | Memory: 82.7 MB (85.33%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -4,7 +4,7 @@ public:
       sort(nums.begin(), nums.end());
 
       for(int i = 1;i<nums.size();i++){
-        if(nums[i] == nums[i - 1]){
+        if(nums[i] == (nums[i - 1])){
             return true;
         }
       }
