06fb9d3746de51625d7a1906c17a5f4ae7249fcf
Naval Singh <navalsingh8321@gmail.com>
2026-07-27 22:53:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (78.10%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
+    bool isPalindrome(string s) {
+            int left = 0;
+    int right = s.length()-1;
 
- bool isalphanum(char ch){
-        if((ch>='0'  &&  ch<='9') || ( tolower(ch)>='a'  && tolower(ch)<='z')){
-            return true;
+    while(left<right){
+        if(!isalnum(s[left])){
+            left++;
+            continue;
         }
+        if(!isalnum(s[right])){
+            right--;
+            continue;
+        }
+
+    if(tolower(s[left]) != tolower(s[right])){
         return false;
     }
-
-    bool isPalindrome(string s) {
-
-        int st = 0; int end = s.length()-1;
-
-      while(st<end){
-        if(!isalphanum(s[st])){
-            st++; continue;
-        }
-        if(!isalphanum(s[end])){
-            end--; continue;
-        }
-        if(tolower(s[st]) != tolower(s[end])){
-                return false;
-            }
-            st++; end--;
-      }
-      return true; 
+    right--;
+    left++;
+}
+    return true;      
     }
 };
