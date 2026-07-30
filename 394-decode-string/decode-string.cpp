36f204d224d31a3309ef731e454e2dbc41a52539
Naval Singh <navalsingh8321@gmail.com>
2026-07-30 23:22:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (52.81%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,30 @@
+// from ai
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> numStack;
+        stack<string> strStack;
+        string currStr = "";
+        int num = 0;
+
+        for (char ch : s) {
+            if (isdigit(ch)) {
+                num = num * 10 + (ch - '0');
+            } else if (ch == '[') {
+                numStack.push(num);
+                strStack.push(currStr);
+                num = 0;
+                currStr = "";
+            } else if (ch == ']') {
+                int repeat = numStack.top(); numStack.pop();
+                string prevStr = strStack.top(); strStack.pop();
+                string temp = "";
+                for (int i = 0; i < repeat; i++) temp += currStr;
+                currStr = prevStr + temp;
+            } else {
+                currStr += ch;
+            }
+        }
+        return currStr;
+    }
+};
