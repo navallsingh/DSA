583704a08cf7ed43f9437bb2d946159b21a538b8
Naval Singh <navalsingh8321@gmail.com>
2026-08-05 22:05:41 +0530

    Time: 1358 ms (6.24%) | Memory: 9.2 MB (95.12%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if (s.size() != t.size()) return false;
+    if( s.size() != t.size()) return false;
 
-        vector<int> mapS(256, -1); // mapping from s to t
-        vector<int> mapT(256, -1); // mapping from t to s
-
-        for (int i = 0; i < s.size(); i++) {
-            char c1 = s[i];
-            char c2 = t[i];
-
-            // If mapping already exists, check consistency
-            if (mapS[c1] != -1 && mapS[c1] != c2) return false;
-            if (mapT[c2] != -1 && mapT[c2] != c1) return false;
-
-            // Otherwise, establish mapping
-            mapS[c1] = c2;
-            mapT[c2] = c1;
+    for(int i = 0;i<s.size();i++){
+        for(int j = i+1;j<s.size();j++){
+            if(s[i]==s[j] && t[i]!=t[j]) return false;
+            if(s[i]!=s[j] && t[i]==t[j]) return false;
         }
-        return true;
+    }
+    return true;
     }
 };
