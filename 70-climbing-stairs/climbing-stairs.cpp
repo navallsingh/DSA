1ff59290bfb9e73d322e84091ca0460b7368fef6
Naval Singh <navalsingh8321@gmail.com>
2026-08-14 00:06:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (74.63%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,11 +1,9 @@
 class Solution {
-    public:
-     int climbStairs(int n) {
-        if (n <= 2) return n;
-        
-        int a = 1, b = 2; // f(1), f(2)
-        for (int i = 3; i <= n; i++) {
-            int c = a + b; // f(i) = f(i-1) + f(i-2)
+public:
+    int climbStairs(int n) {
+        int a = 1, b = 1;
+        for(int i = 2; i <= n; i++) {
+            int c = a + b;
             a = b;
             b = c;
         }
